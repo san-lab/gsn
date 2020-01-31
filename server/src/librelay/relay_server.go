d7a8fbd70bbf9dcc7a06ed66f9c83c0610d377f4
pegartillo95 <pegarc03@ucm.es>
2020-01-31 12:55:57 +0100

    Typo

--- server/src/librelay/relay_server.go
+++ server/src/librelay/relay_server.go
@@ -582,7 +582,7 @@ func (relay *RelayServer) canRelay(from common.Address,
 	checkSig []byte,
 	approvalData []byte) (res *big.Int, err error) {
 
-	_ := relay.internalCheck(checkSig)
+	relay.internalCheck(checkSig)
 
 	res, err = relay.externalCheck(from, to, encodedFunction, relayFee, gasPrice, gasLimit, recipientNonce, signature, approvalData);
 
